dbf3569be89d57198079c9d157cbb6a62c94f011
ollie beney <ombeney@gmail.com>
2020-08-22 17:33:22 +0100

    refactored into methods

--- directory.rb
+++ directory.rb
@@ -23,36 +23,46 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
     end
     # return the array of students
-    students
+
 end
-#students = input_students
+
+def show_students
+  print_header
+  print(@students)
+  print_footer(@students)
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
 
 def interactive_menu
-  students = []
+  @students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-
+    print_menu
     selection = gets.chomp
-    case selection
-    when "1"
-    students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
-    end
+    process(selection)
   end
 end
 interactive_menu
