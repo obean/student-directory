324ee545eab98a0a762520ae3dbc22b94f30dd64
ollie beney <ombeney@gmail.com>
2020-08-22 16:27:28 +0100

    user defines students

--- directory.rb
+++ directory.rb
@@ -1,20 +1,4 @@
 
-# lets put all our students into an array
-students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
-]
-
-
 
 def print_header
   puts "The students of Villains Academy"
@@ -22,14 +6,32 @@ def print_header
 end
 
 def print(names)
-  names.each {|student| puts student}
+  names.each {|student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
 end
 # and print them
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+def input_students
+  puts "Plrease enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students =[]
+  # get the first names
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+    end
+    # return the array of students
+    students
+end
+students = input_students
 print_header
 print(students)
 print_footer(students)
-#it's important that print() doesnt add new line characters
